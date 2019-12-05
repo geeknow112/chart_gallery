121ec3383296771bb5e8794a1a43dd4e58f0a892
myu <yourenemy1008@gmail.com>
2019-12-05 10:36:47 +0900

    add zip function

--- ConsoleApp2/Module1.vb
+++ ConsoleApp2/Module1.vb
@@ -4,23 +4,48 @@
     Dim csvPath As String = "C:\Users\r2d2\source\repos\chart_gallery\stock_data\"
 
     Sub Main()
+        'makeZip()
+        'Exit Sub
+
         'getCmdParam()
 
         Dim securitiesCodes() As Integer = {
-            9101, 9104, 9107, 9984
+            9101
         }
+        'Dim securitiesCodes() As Integer = {
+        '    9101, 9104, 9107, '海運業
+        '    4021, '日産化
+        '    4183, '三井化学
+        '    4005, '住友化
+        '    4188, '三菱ケミＨＤ
+        '    4911, '資生堂
+        '    3407, '旭化成
+        '    4042, '東ソー
+        '    6988, '日東電
+        '    3405, 'クラレ
+        '    4061, 'デンカ
+        '    4208, '宇部興
+        '    4272, '日化薬
+        '    4004, '昭電工
+        '    4631, 'ＤＩＣ
+        '    4043, 'トクヤマ
+        '    4901, '富士フイルム
+        '    4452, '花王
+        '    4063, '信越化
+        '    9984  'ソフトバンクグループ
+        '}
 
         Dim Prices As New ActiveMarket.Prices
         Dim Calendar As New ActiveMarket.Calendar
         Dim hash As New Hashtable
         Dim date_position As Integer
-        Dim date_range As Integer
+        Dim date_range, test As Integer
         Dim output As String
         Dim csvFile As String
 
         For securitiesCode = 0 To UBound(securitiesCodes)
             Prices.Read(securitiesCodes(securitiesCode))
-            date_position = 7900
+            date_position = Prices.Begin() '5632?
             date_range = Prices.End - date_position
             Dim stock_array(date_range, 7) As String
 
@@ -29,6 +54,7 @@
                 If Prices.IsClosed(date_position) Then
                     Continue For
                 End If
+
                 stock_array(i, 0) = Format(Calendar.Date(date_position), "yyyy-MM-dd")
                 stock_array(i, 1) = Prices.Open(date_position)
                 stock_array(i, 2) = Prices.High(date_position)
@@ -36,6 +62,7 @@
                 stock_array(i, 4) = Prices.Close(date_position)
                 stock_array(i, 5) = Math.Floor(Prices.Volume(date_position) * 1000)
                 stock_array(i, 6) = Prices.Close(date_position)
+                stock_array(i, 7) = date_position
             Next
 
             Try
@@ -44,14 +71,14 @@
 
             hash.Add(securitiesCodes(securitiesCode), Prices.Name)
 
-            csvFile = csvPath + CType(securitiesCodes(securitiesCode), String) + "_2019.csv"
+            csvFile = csvPath + CType(securitiesCodes(securitiesCode), String) + ".csv"
 
             'ファイル削除
             System.IO.File.Delete(csvFile)
 
             'csvヘッダー表示
             outputCsv(CType(securitiesCodes(securitiesCode), String) + " " + Prices.Name + ",,,,," + vbCrLf _
-                    + """date"",""open"",""hight"",""low"",""close"",""power"",""End""", csvFile)
+                    + """date"",""open"",""hight"",""low"",""close"",""power"",""End"",""date_position""", csvFile)
 
             For i = 0 To date_range
                 If IsNothing(stock_array(i, 0)) Then
@@ -59,7 +86,7 @@
                 End If
 
                 output = """" + stock_array(i, 0) + """,""" + stock_array(i, 1) + """,""" + stock_array(i, 2) + """,""" + stock_array(i, 3) _
-                     + """,""" + stock_array(i, 4) + """,""" + stock_array(i, 5) + """,""" + stock_array(i, 6) + """"
+                     + """,""" + stock_array(i, 4) + """,""" + stock_array(i, 5) + """,""" + stock_array(i, 6) + """,""" + stock_array(i, 7) + """"
                 'System.Diagnostics.Debug.WriteLine(output)
                 outputCsv(output, csvFile)
             Next
@@ -82,6 +109,11 @@
         For Each cmd In cmds
             Console.WriteLine(cmd)
         Next
+    End Sub
+
+    Sub makeZip()
+        'ZIP書庫を作成
+        System.IO.Compression.ZipFile.CreateFromDirectory("C:\temp\test\dir", "C:\temp\test\1.zip", System.IO.Compression.CompressionLevel.Optimal, False, System.Text.Encoding.GetEncoding("shift_jis"))
     End Sub
 
 End Module

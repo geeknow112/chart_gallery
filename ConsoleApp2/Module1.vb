67eb9b500a96fedc33b7f50f3a3b821db4250a46
myu <yourenemy1008@gmail.com>
2020-08-25 23:19:06 +0900

    connect mysql

--- ConsoleApp2/Module1.vb
+++ ConsoleApp2/Module1.vb
@@ -1,45 +1,27 @@
-﻿Module Module1
+﻿Imports MySql.Data.MySqlClient
+Module Module1
 
     'Dim csvPath As String = "D:\Users\z112\source\repos\ConsoleApp2\stock_data\"
     Dim csvPath As String = "C:\Users\r2d2\source\repos\chart_gallery\stock_data\"
+    'ReadOnly csvPath As String = "C:\Users\r2d2\source\repos\chart_gallery\stock_data\"
 
     Sub Main()
-        'makeZip()
+        'ConnectMySql()
+
+        'MakeZip()
         'Exit Sub
 
-        'getCmdParam()
+        'GetCmdParam()
 
         Dim securitiesCodes() As Integer = {
-            9101, 7186
+            9101, 6326
         }
-        'Dim securitiesCodes() As Integer = {
-        '    9101, 9104, 9107, '海運業
-        '    4021, '日産化
-        '    4183, '三井化学
-        '    4005, '住友化
-        '    4188, '三菱ケミＨＤ
-        '    4911, '資生堂
-        '    3407, '旭化成
-        '    4042, '東ソー
-        '    6988, '日東電
-        '    3405, 'クラレ
-        '    4061, 'デンカ
-        '    4208, '宇部興
-        '    4272, '日化薬
-        '    4004, '昭電工
-        '    4631, 'ＤＩＣ
-        '    4043, 'トクヤマ
-        '    4901, '富士フイルム
-        '    4452, '花王
-        '    4063, '信越化
-        '    9984  'ソフトバンクグループ
-        '}
 
         Dim Prices As New ActiveMarket.Prices
         Dim Calendar As New ActiveMarket.Calendar
         Dim hash As New Hashtable
         Dim date_position As Integer
-        Dim date_range, test As Integer
+        Dim date_range As Integer
         Dim output As String
         Dim csvFile As String
 
@@ -54,8 +36,9 @@
             If System.IO.File.Exists(csvFile) Then
                 '存在したら最終行(改行を含まず)のdate_positionの数値を取得
                 Dim enc As System.Text.Encoding = System.Text.Encoding.GetEncoding("Shift_JIS") 'CSVファイルのエンコードを指定（今回はShift_JIS）
-                Dim lineCount As Integer = UBound(My.Computer.FileSystem.OpenTextFileReader(csvFile).ReadToEnd.Split(Chr(13)))
-                Dim srr As New System.IO.StreamReader(csvFile) '読み込むファイルを開く
+                Dim lineCount As Integer = UBound(My.Computer.FileSystem.OpenTextFileReader(csvFile).ReadToEnd.Split(Chr(13))) - 2 '先頭2行(銘柄名、ヘッダ)を除外
+                Dim fs As New System.IO.FileStream(csvFile, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.ReadWrite)
+                Dim srr As New System.IO.StreamReader(fs, enc) '読み込むファイルを開く
                 Dim line, posi As String
                 line = srr.ReadLine()
                 For i As Integer = 1 To lineCount - 1
@@ -71,7 +54,7 @@
                 date_position = Prices.Begin() '5632?
 
                 'csvヘッダー表示
-                outputCsv(CType(securitiesCodes(securitiesCode), String) + " " + Prices.Name + ",,,,," + vbCrLf _
+                OutputCsv(CType(securitiesCodes(securitiesCode), String) + " " + Prices.Name + ",,,,," + vbCrLf _
                     + """date"",""open"",""hight"",""low"",""close"",""power"",""End"",""date_position""", csvFile)
             End If
 
@@ -109,21 +92,21 @@
                 output = """" + stock_array(i, 0) + """,""" + stock_array(i, 1) + """,""" + stock_array(i, 2) + """,""" + stock_array(i, 3) _
                      + """,""" + stock_array(i, 4) + """,""" + stock_array(i, 5) + """,""" + stock_array(i, 6) + """,""" + stock_array(i, 7) + """"
                 'System.Diagnostics.Debug.WriteLine(output)
-                outputCsv(output, csvFile)
+                OutputCsv(output, csvFile)
             Next
         Next
 
     End Sub
 
-    Sub outputCsv(output, csvFile)
+    Sub OutputCsv(output, csvFile)
         Dim enc As System.Text.Encoding = System.Text.Encoding.GetEncoding("Shift_JIS") 'CSVファイルのエンコードを指定（今回はShift_JIS）
-        Dim sr As New System.IO.StreamWriter(csvFile, True, enc) '書き込むファイルを開く
+        Dim sr As New System.IO.StreamWriter(csvFile + "", True, enc) '書き込むファイルを開く
         sr.Write(output)
         sr.Write(vbCrLf) '改行
         sr.Close()
     End Sub
 
-    Sub getCmdParam()
+    Sub GetCmdParam()
         Console.WriteLine(System.Environment.CommandLine) 'コマンドライン引数を表示する
         Dim cmds As String() = System.Environment.GetCommandLineArgs() 'コマンドライン引数を配列で取得する
         Dim cmd As String 'コマンドライン引数を列挙する
@@ -132,9 +115,29 @@
         Next
     End Sub
 
-    Sub makeZip()
+    Sub MakeZip()
         'ZIP書庫を作成
         'System.IO.Compression.ZipFile.CreateFromDirectory("C:\temp\test\dir", "C:\temp\test\1.zip", System.IO.Compression.CompressionLevel.Optimal, False, System.Text.Encoding.GetEncoding("shift_jis"))
     End Sub
 
+    Sub ConnectMySql()
+        Const DB_Source As String = "localhost"
+        Const DB_Port As String = "3306"
+        Const DB_Name As String = "stocks"
+        Const DB_Id As String = "root"
+        Const DB_Pw As String = "rage5557"
+
+        Using Conn As New MySqlConnection("Database=" + DB_Name _
+                                        + ";Data Source=" + DB_Source _
+                                        + ";Port=" + DB_Port _
+                                        + ";User Id=" + DB_Id _
+                                        + ";Password=" + DB_Pw _
+                                        + ";sqlservermode=True;")
+            Conn.Open()
+            Dim query = "INSERT INTO test VALUES (2, 'test2')"
+            Dim cmd As MySqlCommand = New MySqlCommand(query, Conn)
+            cmd.ExecuteNonQuery()
+            Conn.Close()
+        End Using
+    End Sub
 End Module
